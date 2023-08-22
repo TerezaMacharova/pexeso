3d8c990482a82e13e5f11aff83bfc8ded35bb8e6
Asus <Asus@LAPTOP-AV0QSD1C>
2023-08-22 19:39:39 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace pexeso
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PanelGame = new System.Windows.Forms.FlowLayoutPanel();
+            this.panelChooseCards = new System.Windows.Forms.Panel();
+            this.numericUpDownCards = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelReadyToPlay = new System.Windows.Forms.Label();
+            this.panelLetsPlay = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.labelLetsPlay = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.PanelGame.SuspendLayout();
+            this.panelChooseCards.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCards)).BeginInit();
+            this.panelLetsPlay.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PanelGame
+            // 
+            this.PanelGame.Controls.Add(this.panelChooseCards);
+            this.PanelGame.Controls.Add(this.panelLetsPlay);
+            this.PanelGame.Location = new System.Drawing.Point(12, 12);
+            this.PanelGame.Name = "PanelGame";
+            this.PanelGame.Size = new System.Drawing.Size(764, 562);
+            this.PanelGame.TabIndex = 0;
+            this.PanelGame.Visible = false;
+            // 
+            // panelChooseCards
+            // 
+            this.panelChooseCards.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.panelChooseCards.BackColor = System.Drawing.Color.MistyRose;
+            this.panelChooseCards.Controls.Add(this.numericUpDownCards);
+            this.panelChooseCards.Controls.Add(this.button1);
+            this.panelChooseCards.Controls.Add(this.labelReadyToPlay);
+            this.panelChooseCards.Location = new System.Drawing.Point(3, 3);
+            this.panelChooseCards.Name = "panelChooseCards";
+            this.panelChooseCards.Size = new System.Drawing.Size(413, 234);
+            this.panelChooseCards.TabIndex = 0;
+            this.panelChooseCards.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // numericUpDownCards
+            // 
+            this.numericUpDownCards.Font = new System.Drawing.Font("Baskerville Old Face", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDownCards.ForeColor = System.Drawing.Color.Maroon;
+            this.numericUpDownCards.Increment = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDownCards.Location = new System.Drawing.Point(182, 78);
+            this.numericUpDownCards.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDownCards.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDownCards.Name = "numericUpDownCards";
+            this.numericUpDownCards.Size = new System.Drawing.Size(65, 42);
+            this.numericUpDownCards.TabIndex = 3;
+            this.numericUpDownCards.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDownCards.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Baskerville Old Face", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Maroon;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(158, 145);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 38);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnConfirmCards_Click);
+            // 
+            // labelReadyToPlay
+            // 
+            this.labelReadyToPlay.AutoSize = true;
+            this.labelReadyToPlay.Font = new System.Drawing.Font("Baskerville Old Face", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelReadyToPlay.ForeColor = System.Drawing.Color.Firebrick;
+            this.labelReadyToPlay.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.labelReadyToPlay.Location = new System.Drawing.Point(29, 27);
+            this.labelReadyToPlay.Name = "labelReadyToPlay";
+            this.labelReadyToPlay.Size = new System.Drawing.Size(360, 38);
+            this.labelReadyToPlay.TabIndex = 0;
+            this.labelReadyToPlay.Text = "Choose a number of cards";
+            // 
+            // panelLetsPlay
+            // 
+            this.panelLetsPlay.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.panelLetsPlay.BackColor = System.Drawing.Color.MistyRose;
+            this.panelLetsPlay.Controls.Add(this.button2);
+            this.panelLetsPlay.Controls.Add(this.labelLetsPlay);
+            this.panelLetsPlay.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.panelLetsPlay.Location = new System.Drawing.Point(3, 243);
+            this.panelLetsPlay.Name = "panelLetsPlay";
+            this.panelLetsPlay.Size = new System.Drawing.Size(413, 234);
+            this.panelLetsPlay.TabIndex = 1;
+            this.panelLetsPlay.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Baskerville Old Face", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Maroon;
+            this.button2.Location = new System.Drawing.Point(158, 101);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 35);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "start the game";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnLetsPlay);
+            // 
+            // labelLetsPlay
+            // 
+            this.labelLetsPlay.AutoSize = true;
+            this.labelLetsPlay.Font = new System.Drawing.Font("Baskerville Old Face", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLetsPlay.ForeColor = System.Drawing.Color.Maroon;
+            this.labelLetsPlay.Location = new System.Drawing.Point(97, 43);
+            this.labelLetsPlay.Name = "labelLetsPlay";
+            this.labelLetsPlay.Size = new System.Drawing.Size(219, 34);
+            this.labelLetsPlay.TabIndex = 0;
+            this.labelLetsPlay.Text = "Great! Let\'s play!";
+            this.labelLetsPlay.Click += new System.EventHandler(this.labelLetsPlay_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(793, 615);
+            this.Controls.Add(this.PanelGame);
+            this.Name = "Form1";
+            this.Text = "Pexeso game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.PanelGame.ResumeLayout(false);
+            this.panelChooseCards.ResumeLayout(false);
+            this.panelChooseCards.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCards)).EndInit();
+            this.panelLetsPlay.ResumeLayout(false);
+            this.panelLetsPlay.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel PanelGame;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panelChooseCards;
+        private System.Windows.Forms.Label labelReadyToPlay;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDownCards;
+        private System.Windows.Forms.Panel panelLetsPlay;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label labelLetsPlay;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace pexeso
+{
+    public partial class Form1 : Form
+    {
+        private int NumberOfCards = 20; 
+        private List<Button> cards = new List<Button>();
+        private Button firstCard;
+        private Button secondCard;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Tick += timer1_Tick;
+            timer1.Interval = 650;
+
+            panelChooseCards.Visible = true;
+            panelLetsPlay.Visible = false;
+            PanelGame.Visible = false;
+        }
+
+        private void btnConfirmCards_Click(object sender, EventArgs e)
+        {
+            if (numericUpDownCards.Value % 2 != 0)
+            {
+                MessageBox.Show("Oh, you have chosen an odd number! Please choose even one!", "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            panelChooseCards.Visible = false;
+            panelLetsPlay.Visible = true;
+        }
+
+        private void btnLetsPlay (object sender, EventArgs e)
+        {
+            NumberOfCards = (int)numericUpDownCards.Value;
+           // System.Diagnostics.Debug.WriteLine($"Number of cards: {NumberOfCards}");
+
+
+            panelLetsPlay.Visible = false;
+            PanelGame.Visible = true;
+
+            StartGame();
+        }
+        private void StartGame()
+        {
+            var imageList = new List<Image>
+            {
+                Image.FromFile("resources/img1.jpg"),
+                Image.FromFile("resources/img2.jpg"),
+                Image.FromFile("resources/img3.jpg"),
+                Image.FromFile("resources/img4.jpg"),
+                Image.FromFile("resources/img5.jpg"),
+                Image.FromFile("resources/img6.jpg"),
+                Image.FromFile("resources/img7.jpg"),
+                Image.FromFile("resources/img8.jpg"),
+                Image.FromFile("resources/img9.jpg"),
+                Image.FromFile("resources/img10.png"),
+        };
+
+            for (int i = 0; i < NumberOfCards; i++)
+            {
+                //System.Diagnostics.Debug.WriteLine($"Creating card {i + 1}");
+                var btn = new Button
+                {
+                    Width = 100,
+                    Height = 100,
+                    Tag = new CardInfo
+                    {
+                        Value = i % (NumberOfCards / 2) + 1,
+                        Image = imageList[i % (NumberOfCards / 2)],
+                        IsMatched = false
+                    },
+                    BackColor = Color.LightPink,
+                };
+
+                btn.Click += CardClick;
+                cards.Add(btn);
+                PanelGame.Controls.Add(btn);
+            }
+
+            var rnd = new Random();
+            var shuffledCards = cards.OrderBy(item => rnd.Next()).ToList();
+            PanelGame.Controls.Clear();
+            foreach (var Card in shuffledCards)
+            {
+                PanelGame.Controls.Add(Card);
+            }
+        }
+
+        private void CardClick(object sender, EventArgs e)
+        {
+            var clickedCard = (Button)sender;
+            var cardInfo = (CardInfo)clickedCard.Tag;
+
+            if (clickedCard.BackColor == Color.LightGreen)
+                return;
+
+            if (cardInfo.IsMatched)
+                return;
+
+            clickedCard.BackgroundImage = cardInfo.Image;
+            clickedCard.BackgroundImageLayout = ImageLayout.Stretch;
+
+            if (firstCard == null)
+            {
+                firstCard = clickedCard;
+            }
+            else
+            {
+                secondCard = clickedCard;
+
+                if (((CardInfo)firstCard.Tag).Value == ((CardInfo)secondCard.Tag).Value)
+                {
+                    firstCard.BackColor = Color.Green;
+                    secondCard.BackColor = Color.Green;
+                    ((CardInfo)firstCard.Tag).IsMatched = true;
+                    ((CardInfo)secondCard.Tag).IsMatched = true;
+                    firstCard = null;
+                    secondCard = null;
+                }
+                else
+                {
+                    timer1.Start();
+                }
+            }
+
+            if (AllPairsAreFound())
+            {
+                DialogResult result = MessageBox.Show("YOU WON! Do you want to play again?", "GAME OVER", MessageBoxButtons.YesNo);
+                
+            if (result == DialogResult.Yes)
+                {
+                    restartGameButton_Click(this, new EventArgs());
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            firstCard.BackgroundImage = null;
+            secondCard.BackgroundImage = null;
+            firstCard.BackColor = Color.LightPink;
+            secondCard.BackColor = Color.LightPink;
+            firstCard = null;
+            secondCard = null;
+            timer1.Stop();
+        }
+
+        private void restartGameButton_Click (object sender, EventArgs e)
+        {
+            cards.Clear();
+            PanelGame.Controls.Clear();
+            firstCard = null;
+            secondCard= null;
+            StartGame();
+        }
+
+        private bool AllPairsAreFound()
+        {
+            return cards.All(Card => ((CardInfo)Card.Tag).IsMatched);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void labelLetsPlay_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class CardInfo
+    {
+        public int Value { get; set; }
+        public Image Image { get; set; }
+        public bool IsMatched { get; set; }
+    }
+}
+
